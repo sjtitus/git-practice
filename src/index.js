512629f6e331c7a651424444eb963d08dd87f5e8
Stephen Titus <sjtitus@alumni.duke.edu>
2021-05-23 17:25:24 -0400

    get the line conflicting

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ console.log(`Hello World!`);
 console.log(`Hello Master 3!`);
 console.log(`Hello Master 4!`);
 console.log(`Hello Master 5!`);
+
 // this is a change on feature 2: here is my comment that we'll conflict
